5f413e0deadeaff5b857fe7f0daa60430450a9ca
Aaron Pittenger <pittenger.aaron@gmail.com>
2019-05-22 21:15:34 -0400

    Fixed the return message associated with the login

--- app/src/main/java/com/example/openwrt/RouterConnection.kt
+++ app/src/main/java/com/example/openwrt/RouterConnection.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.gson.responseObject
+import com.github.kittinunf.result.Result
 
 class RouterConnection(private var info: RouterInfo, private var uname: String, private var pwd: String, private var listener: OnNetworkElementChange){
 
@@ -28,23 +29,26 @@ class RouterConnection(private var info: RouterInfo, private var uname: String, 
         Fuel.post("http://${info.ipString}/cgi-bin/luci/rpc/auth")
             .jsonBody("{\"id\": 1, \"method\": \"login\", \"params\": [\"$uname\", \"$pwd\"]}")
             .responseObject<LoginResponse> { request, response, result ->
-                Log.v("Response", request.toString())
-                Log.v("Response", response.responseMessage)
-                Log.v("Response", result.toString())
-                Log.v("Response", result.component1().toString())
+                when (result) {
+                    is Result.Success -> {
+                        val (data, err) = result
+                        if (data?.error.isNullOrEmpty() and !data?.result.isNullOrEmpty()) {
+                            this.key = data?.result
+                            Log.v("Response", this.key.toString())
+                            this.connected = true
+                            listener.onNetworkElementChange(true, info)
+                        } else {
+                            Log.v("Response", "Failed Connection!")
+                            this.key = null
+                            this.failed = true
+                            listener.onNetworkElementChange(false, info)
+                        }
+                    }
+                    is Result.Failure -> {
+                        listener.onNetworkElementChange(false, info)
+                    }
 
-                if(result.component1()?.error.equals(null) and !result.component1()?.result.equals(null)){
-                    this.key = result.component1()?.result
-                    Log.v("Response", this.key.toString())
-                    this.connected = true
-                    listener.onNetworkElementChange(true, info)
-                }else{
-                    Log.v("Response", "Failed Connection!")
-                    this.key = null
-                    this.failed = true
-                    listener.onNetworkElementChange(false, info)
                 }
-
             }
     }
 }
